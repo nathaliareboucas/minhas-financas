4735b6371fed04b57ca6975ca56c939a824e451f
Nathalia <nathreboucas5@gmail.com>
2019-11-10 17:14:34 -0300

    melhorando os tetes de integração - usuarioRepository

--- src/test/java/com/nathaliareboucas/minhasfinancas/model/repository/UsuarioRepositoryTest.java
+++ src/test/java/com/nathaliareboucas/minhasfinancas/model/repository/UsuarioRepositoryTest.java
@@ -4,27 +4,48 @@ import org.assertj.core.api.Assertions;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase.Replace;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.boot.test.autoconfigure.orm.jpa.TestEntityManager;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.junit4.SpringRunner;
 
 import com.nathaliareboucas.minhasfinancas.model.entity.Usuario;
 
-@SpringBootTest
 @RunWith(SpringRunner.class)
 @ActiveProfiles("test")
+@DataJpaTest
+@AutoConfigureTestDatabase(replace = Replace.NONE)
 public class UsuarioRepositoryTest {
 
 	@Autowired
 	UsuarioRepository repository;
+	@Autowired
+	TestEntityManager entityManager;
 	
 	@Test
 	public void verificaExistenciaEmailCadastrado() {
+		// cenário
 		Usuario usuario = Usuario.builder().nome("João").email("joao@gmail.com").build();
-		repository.save(usuario);
+		entityManager.persist(usuario);
 		
+		// ação
 		boolean existe = repository.existsByEmail("joao@gmail.com");
+		
+		// verificação
 		Assertions.assertThat(existe).isTrue();
 		
+	}
+	
+	@Test
+	public void deveRetornarFalsoQuandoNaoHouverUsuarioCadastradoComOEmail() {
+		// cenário
+		
+		// ação
+		boolean existe = repository.existsByEmail("joao@gmail.com");
+		
+		// verificação
+		Assertions.assertThat(existe).isFalse();
 	}
 }
