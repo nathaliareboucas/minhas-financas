8d008e45597291ec69dc0f067156eed4c6568128
Nathalia <nathreboucas5@gmail.com>
2020-01-13 22:04:45 -0300

    criando a busca de lançamento por usuário e tipo de lançamento

--- src/main/java/com/nathaliareboucas/minhasfinancas/model/repository/LancamentoRepository.java
+++ src/main/java/com/nathaliareboucas/minhasfinancas/model/repository/LancamentoRepository.java
@@ -1,9 +1,21 @@
 package com.nathaliareboucas.minhasfinancas.model.repository;
 
+import java.math.BigDecimal;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
 
 import com.nathaliareboucas.minhasfinancas.model.entity.Lancamento;
+import com.nathaliareboucas.minhasfinancas.model.enums.TipoLancamento;
 
-public interface LancamentoRepository extends JpaRepository<Lancamento, Long>{
+@Repository
+public interface LancamentoRepository extends JpaRepository<Lancamento, Long> {
+	
+	@Query(value = 
+			"select sum(l.valor) from Lancamento l join l.usuario u "
+		  + "where u.id = :usuarioId and l.tipo = :tipoLancamento group by u")
+	BigDecimal obterSaldoPorTipoLancamentoUsuario(@Param("usuarioId") Long usuarioId, @Param("tipoLancamento") TipoLancamento tipoLancamento);
 
 }
