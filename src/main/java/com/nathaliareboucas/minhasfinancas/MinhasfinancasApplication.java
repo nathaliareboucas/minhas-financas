12cb5fc3c001b84c67118e0b1fc97c407f0444e5
Nathalia <nathreboucas5@gmail.com>
2020-01-25 20:23:08 -0300

    habilitando o CORS

--- src/main/java/com/nathaliareboucas/minhasfinancas/MinhasfinancasApplication.java
+++ src/main/java/com/nathaliareboucas/minhasfinancas/MinhasfinancasApplication.java
@@ -2,9 +2,19 @@ package com.nathaliareboucas.minhasfinancas;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
-public class MinhasfinancasApplication {
+@EnableWebMvc
+public class MinhasfinancasApplication implements WebMvcConfigurer {
+	
+	@Override
+	public void addCorsMappings(CorsRegistry registry) {
+		registry.addMapping("/**").allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS");
+		WebMvcConfigurer.super.addCorsMappings(registry);
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(MinhasfinancasApplication.class, args);
